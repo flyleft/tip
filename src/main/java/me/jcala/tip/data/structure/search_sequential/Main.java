4d6755701f86e757da22ac93622384e2f929cfac
jcala <jcalaz@163.com>
2017-03-18 18:52:37 +0800

    创建了数据结构-查找

---
+++ src/main/java/me/jcala/tip/data/structure/search_sequential/Main.java
@@ -0,0 +1,12 @@
+package me.jcala.tip.data.structure.search_sequential;
+
+public class Main {
+    public int search(int[] array,int which){
+        for (int i=0;i<array.length;i++){
+            if (array[i]==which){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
